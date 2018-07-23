cdbc59f3c2dad7e0200c99bf942bc8e02592b179
Amit Upadhyay <amit.upadhyay@cleartax.in>
2018-07-23 08:27:40 +0530

    routes added

---
+++ BinoApp/BinoApp/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using BinoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BinoApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Raazi" };
+            return View(movie);
+        }
+    }
+}
---
+++ BinoApp/BinoApp/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BinoApp.Models
+{
+    public class Movie
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
